37cdf86a4efbbf8af46cb88e85ff7bf11416bb07
Your Name <geramargonzalez@gmail.com>
2017-05-22 18:40:01 -0300

    agregamos spritey movimientos a adam

--- stado0.js
+++ stado0.js
@@ -1,18 +1,51 @@
-var demo = {};
 
+var demo = {};
+var centerx = 1500/2;
+var centery = 1000/2;
+var adam;
+var speed = 4;
 demo.state0 = function (){}
 
 demo.state0.prototype = {
-  preload: function(){},
+  preload: function(){
+    juego.load.image('adam','assets/sprites/adam.png');
+  },
   create:  function(){
-    juego.stage.backgroundColor = '#80ffd4';
+    juego.stage.backgroundColor = '#80ddd4';
     //juego.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(cambiarEstado,null,null,1);
     //juego.input.keyboard.addKey(Phaser.Keyboard.TWO).onDown.add(cambiarEstado,null,null,2);
     addChangeStateListeners();
     juego.scale.scaleMode = Phaser.ScaleManager.SHOW_All;
+    adam = juego.add.sprite(centerx,centery,'adam');
+    //adam.anchor.x = 0.5;
+    //adam.anchor.y = 0.5;
+    adam.anchor.setTo(0.5,0.5);
 
   },
-  update: function(){}
+  update: function(){
+    if (juego.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+    {
+        adam.x += speed;
+        //ufo.angle = -15;
+    }
+    else if (juego.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+    {
+        adam.x -= speed;
+      //  ufo.angle = 15;
+    }
+    if (juego.input.keyboard.isDown(Phaser.Keyboard.DOWN))
+    {
+        adam.y += speed;
+        //ufo.angle = -15;
+    }
+    else if (juego.input.keyboard.isDown(Phaser.Keyboard.UP))
+    {
+        adam.y -= speed;
+      //  ufo.angle = 15;
+    }
+
+
+  }
 
 }
 
