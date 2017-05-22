7b1b0088c067c340c28f9816ee9fd454008540e4
Your Name <geramargonzalez@gmail.com>
2017-05-22 20:07:22 -0300

    agregar fisica y camera

--- stado0.js
+++ stado0.js
@@ -4,43 +4,59 @@ var centerx = 1500/2;
 var centery = 1000/2;
 var adam;
 var speed = 4;
+var tree;
 demo.state0 = function (){}
 
 demo.state0.prototype = {
   preload: function(){
     juego.load.image('adam','assets/sprites/adam.png');
+    juego.load.image('tree','assets/backgrounds/treeBG.png');
   },
   create:  function(){
+    juego.physics.startSystem(Phaser.Physics.ARCADE);
     juego.stage.backgroundColor = '#80ddd4';
+    juego.world.setBounds(0,0,2813,1000);
     //juego.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(cambiarEstado,null,null,1);
     //juego.input.keyboard.addKey(Phaser.Keyboard.TWO).onDown.add(cambiarEstado,null,null,2);
     addChangeStateListeners();
     juego.scale.scaleMode = Phaser.ScaleManager.SHOW_All;
+    tree = juego.add.sprite(0,0,'tree');
     adam = juego.add.sprite(centerx,centery,'adam');
     //adam.anchor.x = 0.5;
     //adam.anchor.y = 0.5;
     adam.anchor.setTo(0.5,0.5);
+    adam.scale.setTo(0.7,0.7);
+    juego.camera.follow(adam);
+    juego.physics.enable(adam);
+    adam.body.collideWorldBounds = true;
+    juego.camera.deadzone = new Phaser.Rectangle(centerx-300,0,600,1000);
 
   },
   update: function(){
     if (juego.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
     {
         adam.x += speed;
+        adam.scale.setTo(0.7,0.7);
         //ufo.angle = -15;
     }
     else if (juego.input.keyboard.isDown(Phaser.Keyboard.LEFT))
     {
         adam.x -= speed;
+        adam.scale.setTo(-0.7,0.7);
       //  ufo.angle = 15;
     }
     if (juego.input.keyboard.isDown(Phaser.Keyboard.DOWN))
     {
         adam.y += speed;
+
         //ufo.angle = -15;
     }
     else if (juego.input.keyboard.isDown(Phaser.Keyboard.UP))
     {
-        adam.y -= speed;
+
+        if(adam.y > 381.79999999999995){
+          adam.y -= speed;
+        }
       //  ufo.angle = 15;
     }
 
